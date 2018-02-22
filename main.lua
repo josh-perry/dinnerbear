5eed40dc89f6657516208813daef4d472da1c9a1
Josh Perry <joshperry@outlook.com>
2018-02-22 19:54:08 +0000

    Added debug hooks

--- main.lua
+++ main.lua
@@ -4,7 +4,19 @@ local Game = require("Game")
 require("MainMenu")
 require("Drinks")
 
+local function initDebug()
+	-- ST3 output
+	io.stdout:setvbuf("no")
+
+	-- ZeroBrane debugging
+	if arg[#arg] == "-debug" then
+		require("mobdebug").start()
+	end
+end
+
 function love.load()
+	initDebug()
+
 	Game = Game:new()
 	Game:changeState("MainMenu")
 end
